6c7b27ea3ba9f2da6edd04ba6a4c6e46cd5274af
RobertoVillani <robertovillani58@gmail.com>
2021-12-16 16:56:21 +0100

    init + header

---
+++ src/components/macro/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="container">
+      <img src="../../assets/img/logo.png" alt="" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 80px;
+  background-color: #2e3a46;
+  line-height: 80px;
+
+  img {
+    height: 80%;
+    vertical-align: middle;
+    margin-left: 20px;
+  }
+}
+</style>
