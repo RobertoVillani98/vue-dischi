a2dc52a02ec3df797a2c166ba742b8dbc6747704
RobertoVillani <robertovillani58@gmail.com>
2021-12-16 18:14:35 +0100

    main

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,12 +1,53 @@
 <template>
-  <main></main>
+  <main>
+    <div class="disk-list row">
+      <DiskCard v-for="(disk, index) in disks" :key="index" :info="disk" />
+    </div>
+  </main>
 </template>
 
 <script>
+import axios from "axios";
+import DiskCard from "../parts/DiskCard.vue";
+
 export default {
   name: "Main",
+  components: {
+    DiskCard,
+  },
+  data() {
+    return {
+      disks: null,
+    };
+  },
+  created() {
+    // get disks array
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        // handle success
+        this.disks = response.data.response;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+main {
+  background-color: #1e2d3b;
+  min-height: calc(100vh - 80px);
+}
+
+.disk-list > * {
+  width: calc(100% / 5 - 40px);
+  margin: 30px 20px;
+}
 </style>
---
+++ src/components/parts/DiskCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <img :src="info.poster" :alt="info.title" />
+    <h3>{{ info.title }}</h3>
+    <p>{{ info.author }}</p>
+    <p>{{ info.year }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DiskCard",
+  props: {
+    info: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  background-color: #2e3a46;
+  text-align: center;
+  padding: 20px;
+  border-radius: 40px;
+  img {
+    width: 100%;
+    border-radius: 40px;
+  }
+  h3 {
+    color: #fff;
+    text-transform: uppercase;
+    margin-top: 10px;
+    margin-bottom: 20px;
+  }
+  p {
+    color: #7e7f7d;
+  }
+}
+</style>
